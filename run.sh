2df6d5a43529782d98f977f89fffb3c9cf9f7dc6
xy <xyag.902@163.com>
2024-06-05 07:30:54 +0800

    备份。

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 export LD_LIBRARY_PATH=library/lib:${LD_LIBRARY_PATH}
 #cp library_src/wcstools-3.9.7/libwcs/*.a library/wcstools-3.8.5/lib/
-make clean -f Makefile2
-make -f Makefile2
+make clean -f Makefile
+make -f Makefile
 ./createtable -d 20160402
 ./createtable -c 20160402
 ./crossmatch -method plane -grid 4,4 -errorRadius 1.5 -searchRadius 50 -fitsHDU 3 -ref  data/ref.Fcat -sample data/M3_05_141025_1_074020_0873.Fcat -dbConfigFile config/database_config.txt -width 3016 -height 3016
